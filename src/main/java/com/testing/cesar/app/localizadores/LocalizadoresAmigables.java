d005061cf72e69d0b48989a087502608ba8149d3
Cesar Munoz <cesarmu@hotmail.com>
2024-11-21 00:08:30 -0500

    Se agrega la clase para los localizadores amigables

--- src/main/java/com/testing/cesar/app/localizadores/LocalizadoresAmigables.java
+++ src/main/java/com/testing/cesar/app/localizadores/LocalizadoresAmigables.java
@@ -3,19 +3,16 @@ package com.testing.cesar.app.localizadores;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
-public class LocalizadoresTradicionales {
+public class LocalizadoresAmigables {
 	public static void main(String[] args) {
 		System.setProperty(
 				"webdriver.gecko.driver",
 				"D:\\Estudio\\Udemy\\TestingWorkspaceSTS\\Drivers\\20 de noviembre 2024\\geckodriver-v0.35.0-win64\\geckodriver.exe");
 		WebDriver driverFirefox = new FirefoxDriver();
 		driverFirefox.get("https://www.google.com/");
-		driverFirefox.findElement(By.id("APjFqb"));		
-		driverFirefox.findElement(By.className("gLFyf"));
-				
-		
-		driverFirefox.quit();
+		driverFirefox.findElement(RelativeLocator.with(By.tagName("a")).toLeftOf(By.className("gb_A"))).click();
 	}
 
 }
